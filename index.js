f64bc4ef01e4707398094cf0f6f62e47804dd58f
Emmanuel Montoya Lopez <emmanuelprogramador24@gmail.com>
2025-06-01 21:48:12 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", ()=>{
+//   let btn = document.querySelector(".btn1");
+//   btn.addEventListener("click", ()=>{
+//     alert("Clickeado");
+//   })
+// })
